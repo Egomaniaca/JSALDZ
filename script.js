e26835d396f4857f1fa643785fba0374edb2d27b
Egomaniaca <egomaniaca@gmail.com>
2022-03-24 11:49:00 +0300

    done

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-
-
-const goods = [
+/*const goods = [
     { title: 'Shirt', price: 150 },
     { title: 'Socks', price: 50 },
     { title: 'Jacket', price: 350 },
@@ -11,8 +9,78 @@ const renderGoodsItem = (title = 111, price = 666) => `<div class="goods-item"><
 
 
 const renderGoodsList = (list) => {
-    let goodsList = (list.map(item => renderGoodsItem(item.title, item.price))).join(' ');
+    let goodsList = list.map(item => renderGoodsItem(item.title, item.price)).join(' ');
     document.querySelector('.goods-list').innerHTML = goodsList;
 }
 
-renderGoodsList(goods);
+renderGoodsList(goods);*/
+
+class GoodsItem {
+    constructor(title, price) {
+        this.title = title;
+        this.price = price;
+    }
+
+    render() {
+
+        return `<div class="goods-item">
+                    <div><img src="https://via.placeholder.com/200"></div>
+                    <h3>${this.title}</h3>
+                    <p>${this.price}</p>
+                    <button class="buy-btn">Купить</button>
+                </div>`
+    }
+}
+
+class GoodList {
+    constructor() {
+        this.goods = [];
+    }
+    fetchGoods() {
+        this.goods = [
+            { title: 'Shirt', price: 150 },
+            { title: 'Socks', price: 50 },
+            { title: 'Jacket', price: 350 },
+            { title: 'Shoes', price: 250 },
+        ];
+    }
+    culcSum() {
+        let sum = 0;
+        this.goods.forEach(good => {
+            sum += good.price;
+        });
+    }
+    render() {
+        let listHtml = '';
+        this.goods.forEach(good => {
+            const goodItem = new GoodsItem(good.title, good.price);
+            listHtml += goodItem.render();
+        });
+        document.querySelector('.goods-list').innerHTML = listHtml;
+    }
+}
+
+const list = new GoodList();
+list.fetchGoods();
+list.render();
+list.culcSum();
+
+class Cart {
+    addGood() {
+
+    }
+    removeGood() {
+
+    }
+
+    render() {
+
+    }
+}
+
+class ElemCart {
+
+    render() {
+
+    }
+}
